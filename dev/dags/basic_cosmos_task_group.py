3d802a393bd46eb2883c980b71ced41adaa49252
Pankaj <pankaj.singh@astronomer.io>
2024-05-24 14:22:36 +0530

    Fix

--- dev/dags/basic_cosmos_task_group.py
+++ dev/dags/basic_cosmos_task_group.py
@@ -46,7 +46,7 @@ def basic_cosmos_task_group() -> None:
         project_config=ProjectConfig(
             (DBT_ROOT_PATH / "jaffle_shop").as_posix(),
         ),
-        render_config=RenderConfig(select=["path:seeds/raw_customers.csv"], enable_mock_profile=False),
+        render_config=RenderConfig(select=["path:seeds/raw_customers.csv"], enable_mock_profile=True),
         execution_config=shared_execution_config,
         operator_args={"install_deps": True},
         profile_config=profile_config,
@@ -60,7 +60,7 @@ def basic_cosmos_task_group() -> None:
         ),
         render_config=RenderConfig(
             select=["path:seeds/raw_orders.csv"],
-            enable_mock_profile=False,  # This is necessary to benefit from partial parsing when using ProfileMapping
+            enable_mock_profile=True,  # This is necessary to benefit from partial parsing when using ProfileMapping
         ),
         execution_config=shared_execution_config,
         operator_args={"install_deps": True},
